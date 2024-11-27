9be52f03981deaf13c7ae8f82242ef0029631ae1
Rachid <rachid.saba@ec.gc.ca>
2024-11-27 03:29:17 +0000

    feature dark-mode added

---
+++ myPf.js
@@ -0,0 +1,53 @@
+// Sélectionner le bouton et le body
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+const li = document.getElementsByTagName('li');
+const h2 = document.getElementsByTagName('h2');
+const h3 = document.getElementsByTagName('h3');
+const cv = document.getElementsByClassName('CV');
+const logo = document.getElementById('Logo');
+
+function toggleClassOnElements(elements, className, add) {
+    for (let element of elements) {
+        if (add) {
+            element.classList.add(className);
+        } else {
+            element.classList.remove(className);
+        }
+    }
+}
+
+// Vérifier si le mode sombre est déjà activé (via localStorage)
+if (localStorage.getItem('darkMode') === 'enabled') {
+    body.classList.add('dark-mode');
+    toggleClassOnElements(li, 'dark-mode', true);
+    toggleClassOnElements(h2, 'dark-mode', true);
+    toggleClassOnElements(h3, 'dark-mode', true);
+    toggleClassOnElements(cv, 'dark-mode', true);
+    darkModeToggle.textContent = 'Désactiver le mode sombre';
+}
+
+// Ajouter un gestionnaire d'événements au bouton
+darkModeToggle.addEventListener('click', () => {
+    if (body.classList.contains('dark-mode')) {
+        // Désactiver le mode sombre
+        body.classList.remove('dark-mode');
+        toggleClassOnElements(li, 'dark-mode', false);
+        toggleClassOnElements(h2, 'dark-mode', false);
+        toggleClassOnElements(h3, 'dark-mode', false);
+        toggleClassOnElements(cv, 'dark-mode', false);
+        logo.src = "R white.png";
+        darkModeToggle.textContent = 'Activer le mode sombre';
+        localStorage.setItem('darkMode', 'disabled');
+    } else {
+        // Activer le mode sombre
+        body.classList.add('dark-mode');
+        toggleClassOnElements(li, 'dark-mode', true);
+        toggleClassOnElements(h2, 'dark-mode', true);
+        toggleClassOnElements(h3, 'dark-mode', true);
+        toggleClassOnElements(cv, 'dark-mode', true);
+        logo.src = "R yellow.png";
+        darkModeToggle.textContent = 'Désactiver le mode sombre';
+        localStorage.setItem('darkMode', 'enabled');
+    }
+});
